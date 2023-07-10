af5bb10df44fcd24b90f9b72245b477377c82119
unknown <arixxdelera21@gmail.com>
2023-07-10 23:51:55 +0800

    psuedocode - main window

--- contact_tracing_app.py
+++ contact_tracing_app.py
@@ -1,7 +1,18 @@
 # Delera, Aritz B.
 
 # Import tkinter
+import tkinter as tk
+
 # create the main window
+    # import global wildow
+    # cretae the main window
+    # size and position
+    # background image
+    # Create buttons
+    # Run the GUI main loop
+    # Call the function to create the main window
+
+
 # create add entry
 # create search entry
 # create access entry
