b7ad921902c8978ff0532ee7e26c7fcd9c79701b
unknown <arixxdelera21@gmail.com>
2023-07-11 08:37:23 +0800

    size, position, and background has been modified

--- contact_tracing_app.py
+++ contact_tracing_app.py
@@ -11,7 +11,12 @@ def main_window():
     window = tk.Tk()
     window.title('arix')
     # size and position
+    window.geometry("800x600")
+    window.configure(background='white')
     # background image
+    background_image = tk.PhotoImage(file=r"C:\Users\acer\Downloads\Aritzmetic's COVID-19 CONTACT TRACING APP.png")
+    background_label = tk.Label(window, image=background_image)
+    background_label.place(x=0, y=0, relwidth=1, relheight=1)
     # Create buttons
     # Run the GUI main loop
 
