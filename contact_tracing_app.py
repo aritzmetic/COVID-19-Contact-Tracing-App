03a34628cb34e64b14adcc7b755ac27ef63cb5ae
unknown <arixxdelera21@gmail.com>
2023-07-11 09:11:24 +0800

    buttons has been created; methods has been placed

--- contact_tracing_app.py
+++ contact_tracing_app.py
@@ -3,6 +3,16 @@
 # Import tkinter
 import tkinter as tk
 
+# create add entry
+def add_entry():
+    pass
+# create search entry
+def search_entry():
+    pass
+# create access entry
+def access_entries():
+    pass
+
 # create the main window
 def main_window():
     # import global wildow
@@ -18,6 +28,17 @@ def main_window():
     background_label = tk.Label(window, image=background_image)
     background_label.place(x=0, y=0, relwidth=1, relheight=1)
     # Create buttons
+    button_width = 30
+    button_height = 3
+
+    add_button = tk.Button(window, text='Add Entry', command=add_entry, bg='white', width=button_width, height=button_height, highlightthickness=0)
+    add_button.place(relx=0.134, rely=0.638, anchor=tk.CENTER)
+
+    search_button = tk.Button(window, text='Search Entry', command=search_entry, bg='white', width=button_width, height=button_height, highlightthickness=0)
+    search_button.place(relx=0.394, rely=0.638, anchor=tk.CENTER)
+
+    access_button = tk.Button(window, text='Access Entries', command=access_entries, bg='white', width=button_width, height=button_height, highlightthickness=0)
+    access_button.place(relx=0.644, rely=0.638, anchor=tk.CENTER)
     # Run the GUI main loop
 
     window.mainloop()
@@ -26,7 +47,4 @@ def main_window():
 main_window()
 
 
-# create add entry
-# create search entry
-# create access entry
 
