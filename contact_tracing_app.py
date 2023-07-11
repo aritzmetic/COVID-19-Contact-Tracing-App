c06d3c131ece9c11c45d73ce6ebdbd0a0ca571c8
unknown <arixxdelera21@gmail.com>
2023-07-11 08:30:24 +0800

    window has been created

--- contact_tracing_app.py
+++ contact_tracing_app.py
@@ -4,13 +4,21 @@
 import tkinter as tk
 
 # create the main window
+def main_window():
     # import global wildow
-    # cretae the main window
+    global window
+    # create the main window
+    window = tk.Tk()
+    window.title('arix')
     # size and position
     # background image
     # Create buttons
     # Run the GUI main loop
+
+    window.mainloop()
+
     # Call the function to create the main window
+main_window()
 
 
 # create add entry
