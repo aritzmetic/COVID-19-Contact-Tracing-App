60c6fdffe3dbc8ce9c0f2a3280a7c19fa4253cd8
unknown <arixxdelera21@gmail.com>
2023-07-12 23:13:16 +0800

    other label has been modified

--- add_entry.py
+++ add_entry.py
@@ -28,8 +28,23 @@ class AddEntry:
         self.name_entry.place(x=700, y=160)
         
         # Create age label and blank field
+        age_label = tk.Label(self.add_frame, text='Age:', bg='light blue', font=('Georgia', 14))
+        age_label.place(x=600, y=210)
+        self.age_entry = tk.Entry(self.add_frame, font=('Times New Roman', 12))
+        self.age_entry.place(x=700, y=210)
+
         # Create phone number label and blank field
+        phone_label = tk.Label(self.add_frame, text='Phone Number:', bg='light blue', font=('Georgia', 14))
+        phone_label.place(x=520, y=260)
+        self.phone_entry = tk.Entry(self.add_frame, font=('Times New Roman', 12))
+        self.phone_entry.place(x=700, y=260)
+
         # Create location label and blank field
+        location_label = tk.Label(self.add_frame, text='Location:', bg='light blue', font=('Georgia', 14))
+        location_label.place(x=570, y=310)
+        self.location_entry = tk.Entry(self.add_frame, font=('Times New Roman', 12))
+        self.location_entry.place(x=700, y=310)
+        
         # Create questions 
         # Create submit button
         # create back button
