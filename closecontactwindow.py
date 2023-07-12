10636892adbc20a54da09647eb6f5c927d1ead00
unknown <arixxdelera21@gmail.com>
2023-07-13 00:39:03 +0800

    fixed issues; windows in full screen

--- closecontactwindow.py
+++ closecontactwindow.py
@@ -10,6 +10,9 @@ class CloseContactWindow:
         self.window = tk.Toplevel(parent)
         self.window.title("Close Contact Notification!")
 
+        # Set full screen
+        self.window.attributes("-fullscreen", True)
+
         # bg imaage
         image = Image.open(image_path)
         background_image = ImageTk.PhotoImage(image)
@@ -20,11 +23,11 @@ class CloseContactWindow:
         ok_button = tk.Button(self.window, text="OK", command=self.close_window, bg='yellow', font=('new times roman', 16))
         ok_button.place(x=750, y=700)
 
-# show method
-def show(self):
-    self.window.transient(self.parent)
-    self.parent.wait_window(self.window)
+    # show method
+    def show(self):
+        self.window.transient(self.parent)
+        self.parent.wait_window(self.window)
 
-# close window by destroying it
-def close_window(self):
-    self.window.destroy()
+    # close window by destroying it
+    def close_window(self):
+        self.window.destroy()
